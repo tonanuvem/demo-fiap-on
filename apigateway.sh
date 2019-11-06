a177a56710e7bb49f72cbed775bf3261e3082008
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-11-06 17:06:00 -0300

    Update apigateway.sh

--- apigateway.sh
+++ apigateway.sh
@@ -41,6 +41,7 @@ pe "docker run --rm --network=kong-net -e \"KONG_DATABASE=postgres\" -e \"KONG_P
 pe "docker run -d --name kong --network=kong-net -e \"KONG_DATABASE=postgres\" -e \"KONG_PG_HOST=kong-database\" -e \"KONG_PROXY_ACCESS_LOG=/dev/stdout\" -e \"KONG_ADMIN_ACCESS_LOG=/dev/stdout\" -e \"KONG_PROXY_ERROR_LOG=/dev/stderr\" -e \"KONG_ADMIN_ERROR_LOG=/dev/stderr\" -e \"KONG_ADMIN_LISTEN=0.0.0.0:8001, 0.0.0.0:8444 ssl\" -p 8000:8000 -p 8443:8443 -p 8001:8001 -p 8444:8444 kong:latest"
 pe "docker ps"
 pe "curl http://localhost:8001"
+p ""
 
 # KONGA
 pe "docker run -p 1337:1337 --network kong-net --name konga  -e \"TOKEN_SECRET=chavesecreta\" -d pantsel/konga"
