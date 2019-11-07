c6755b510bfb9d791370613f84cc5a4540f61493
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-11-07 19:13:29 -0300

    Update mesh5_prometheus_grafana.cmd

--- mesh5_prometheus_grafana.cmd
+++ mesh5_prometheus_grafana.cmd
@@ -1,7 +1,9 @@
-kubectl -n istio-system port-forward svc/prometheus 9090:9090
-kubectl -n istio-system port-forward svc/grafana 3000:3000
+cd istio-1.3.0
+start /b kubectl -n istio-system port-forward svc/prometheus 9090:9090
+start /b kubectl -n istio-system port-forward svc/grafana 3000:3000
 kubectl apply -f samples/bookinfo/telemetry/metrics.yaml
 kubectl apply -f samples/bookinfo/telemetry/tcp-metrics.yaml
 :: Acessar 20 vezes a aplicação para gerar as métricas: 
 curl http:/192.168.99.100:31380/productpage?[1-20]
-start "" http://localhost:3000
+start /min chrome http://localhost:3000
+cd ..
