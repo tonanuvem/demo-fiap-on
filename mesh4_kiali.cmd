6028d08dbc02109fc0d082f559735944be57e867
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-11-07 19:08:49 -0300

    Update mesh4_kiali.cmd

--- mesh4_kiali.cmd
+++ mesh4_kiali.cmd
@@ -1,5 +1,5 @@
 cd istio-1.3.0
 kubectl apply -f samples/bookinfo/networking/destination-rule-all.yaml
-kubectl -n istio-system port-forward svc/kiali 20001:20001
+start /b kubectl -n istio-system port-forward svc/kiali 20001:20001
 start /min chrome http://admin:admin@127.0.0.1:20001
 cd ..
