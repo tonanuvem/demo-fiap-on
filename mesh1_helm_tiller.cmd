5bfcfe8848d510a7d59c5225b0454b59bf984de7
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-11-07 18:49:40 -0300

    Update mesh1_helm_tiller.cmd

--- mesh1_helm_tiller.cmd
+++ mesh1_helm_tiller.cmd
@@ -1,6 +1,7 @@
-cd "C:\Users\logonrmlocal\Downloads\istio-1.3.0"
+cd istio-1.3.0
 kubectl apply -f install/kubernetes/helm/helm-service-account.yaml
 helm init --service-account tiller
 helm install install/kubernetes/helm/istio-init --name istio-init --namespace istio-system
 helm list
 kubectl get crds | grep 'istio.io' | wc -l
+cd ..
