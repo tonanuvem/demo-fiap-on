7405ea9261604ed3ea5a6e59401e2902186e2b14
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-11-07 15:43:57 -0300

    Create choco-minikube.cmd

---
+++ choco-minikube.cmd
@@ -0,0 +1,4 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install minikube kubernetes-helm curl wget
+minikube start --memory=12288 --cpus=4 --kubernetes-version=v1.14.2 --vm-driver=virtualbox
+wget https://github.com/istio/istio/releases/download/1.3.0/istio-1.3.0-win.zip
