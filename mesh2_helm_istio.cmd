e1ef9ec8a5cc11a76f96841a680862e3c2680bf4
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-11-07 18:57:17 -0300

    Update mesh2_helm_istio.cmd

--- mesh2_helm_istio.cmd
+++ mesh2_helm_istio.cmd
@@ -1,2 +1,6 @@
+cd istio-1.3.0
+kubectl get crds | grep 'istio.io' | wc -l
+ECHO Verificar se o numero acima = 23
 helm install install/kubernetes/helm/istio --name istio --namespace istio-system --values install/kubernetes/helm/istio/values-istio-demo.yaml --set gateways.istio-ingressgateway.type=NodePort
+cd ..
 minikube dashboard
