045350c9883137d91dc72b2dd1ccb10077f1610f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2019-11-07 19:05:13 -0300

    Update mesh3_bookinfo.cmd

--- mesh3_bookinfo.cmd
+++ mesh3_bookinfo.cmd
@@ -1,5 +1,7 @@
+cd istio-1.3.0
 kubectl label namespace default istio-injection=enabled 
 kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml
 kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
 kubectl get gateway
-start "" http:/192.168.99.100:31380/productpage
+start /min chrome http:/192.168.99.100:31380/productpage
+cd ..
